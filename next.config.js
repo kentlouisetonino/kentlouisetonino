7a6e5e38b4578eb26381d9381f403a5e8477f8c0
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2022-06-25 18:19:03 +0800

    Create a Sidebar

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;
+  },
+};
