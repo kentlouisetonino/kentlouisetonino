b6edb59a5069549c1801f35d1071f6fd6062a0af
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-04-09 20:42:46 +0800

    Create a Navbar

---
+++ src/libs/Navbar/Navbar.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import TitleText from '../TitleText/TitleText';
+
+export default function Navbar() {
+  return (
+    <div className="py-4 px-52 flex justify-between border-green-500 border-dashed border-b-[0.5px]">
+      <div className="motion-safe:animate-pulse">
+        <Link href="/">
+          <TitleText
+            value="@kentlouisetonino"
+            className="font-black hover:text-green-500"
+          />
+        </Link>
+      </div>
+      <div className="flex gap-x-4">
+        <Link href="#">
+          <ul className="leading-[30px] p-[2px] text-white/80 hover:underline hover:text-white underline-offset-8 hover:decoration-green-500">
+            Resume
+          </ul>
+        </Link>
+        <Link href="#">
+          <ul className="leading-[30px] p-[2px] text-white/80 hover:underline hover:text-white underline-offset-8 hover:decoration-green-500">
+            Blogs
+          </ul>
+        </Link>
+        <Link href="#">
+          <ul className="leading-[30px] p-[2px] text-white/80 hover:underline hover:text-white underline-offset-8 hover:decoration-green-500">
+            Contact
+          </ul>
+        </Link>
+      </div>
+    </div>
+  );
+}
