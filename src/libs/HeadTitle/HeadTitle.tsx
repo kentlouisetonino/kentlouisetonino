533b3be7d02ee8a00633f9046eef54a12b7b996b
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-04-09 23:26:06 +0800

    move the head

---
+++ src/libs/HeadTitle/HeadTitle.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+
+export default function HeadTitle() {
+  return (
+    <Head>
+      <title>kentlouisetonino</title>
+      <link rel="shortcut icon" href="/profile.jpeg" />
+    </Head>
+  );
+}
