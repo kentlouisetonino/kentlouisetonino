a551a1aa4226ecd4448cdc2a9e45aa8e64f4e44b
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-04-09 22:08:11 +0800

    home content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,21 @@
 import Navbar from '@/src/libs/Navbar/Navbar';
-import { Fragment } from 'react';
+import TitleText from '@/src/libs/TitleText/TitleText';
 
 export default function Home() {
   return (
-    <Fragment>
+    <div>
       <Navbar />
-    </Fragment>
+      <div className="lg:w-[640px] mx-auto py-40 text-center">
+        <TitleText
+          value="HELLO!"
+          size="xlarge"
+          className="text-white/90 font-black block"
+        />
+        <p className="italic text-white/90">
+          I am Computer Programmer with industry experience in Web Application
+          Development.
+        </p>
+      </div>
+    </div>
   );
 }
