205b78fda0b367035afb2e7e9e6f79ab09a0b6e5
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-04-09 22:52:52 +0800

    add head title and icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Fragment } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>kentlouisetonino</title>
+        <link rel="shortcut icon" href="/profile.jpeg" />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
