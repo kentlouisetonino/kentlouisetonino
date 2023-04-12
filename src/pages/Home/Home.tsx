8823babeafaaa9e9ae08b40679e751eb2dc5413f
Kent Louise Tonino <69438999+kentlouisetonino@users.noreply.github.com>
2023-04-12 20:04:58 +0800

    change the footer (#23)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -17,15 +17,10 @@ export default function Home() {
           Application Development.
         </p>
       </div>
-      <div className="fixed inset-x-0 bottom-0 lg:w-[1008px] p-4 mx-auto flex justify-between border-green-500 border-dashed border-t-[0.5px] opacity-90">
-        {/* address */}
-        <div>
-          <p>Address: Cebu, Central Visayas, Philippines 6014</p>
-          <p>Email: tonino.kentlouise@gmail.com</p>
-          <p>Contact: +63-975-902-4002</p>
-        </div>
-        {/* icons */}
-        <div></div>
+      <div className="fixed inset-x-0 bottom-0 lg:w-[1008px] p-4 mx-auto border-green-500 border-dashed border-t-[0.5px] opacity-90">
+        <p className="w-full text-center">
+          &copy; 2023 Copyright. Developed by Kent Louise Tonino
+        </p>
       </div>
     </Fragment>
   );
