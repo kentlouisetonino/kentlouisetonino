19b973d3516fdf8848972dd11ad523679fb273fc
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-05-07 19:10:54 +0800

    refactor home page

--- src/libs/ui/Bannner/Banner.tsx
+++ src/libs/ui/Bannner/Banner.tsx
@@ -1,15 +1,18 @@
 import TitleText from '../TitleText/TitleText';
 
+const navbarHeight = '67px';
+const footerHeight = '57px';
+
 export default function Banner() {
   return (
     <div
       className="lg:w-[640px] mx-auto text-center"
-      style={{ height: 'calc(100vh - 67px - 57px)' }}
+      style={{ height: `calc(100vh - ${navbarHeight} - ${footerHeight})` }}
     >
       <TitleText
         value="HELLO!"
         size="xlarge"
-        className="text-white/90 font-black"
+        className="text-white/90 font-black block"
       />
       <p className="italic text-white/90">
         I&#39;m a Backend Developer with over 2 years of experience in the
