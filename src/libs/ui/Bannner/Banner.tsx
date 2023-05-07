aa17a2046a0a8ba542cf1a74810dbf6dd70a83e8
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-05-07 19:23:37 +0800

    fix the landing page

--- src/libs/ui/Bannner/Banner.tsx
+++ src/libs/ui/Bannner/Banner.tsx
@@ -6,18 +6,20 @@ const footerHeight = '57px';
 export default function Banner() {
   return (
     <div
-      className="lg:w-[640px] mx-auto text-center"
+      className="lg:w-[640px] mx-auto flex align-middle items-center"
       style={{ height: `calc(100vh - ${navbarHeight} - ${footerHeight})` }}
     >
-      <TitleText
-        value="HELLO!"
-        size="xlarge"
-        className="text-white/90 font-black block"
-      />
-      <p className="italic text-white/90">
-        I&#39;m a Backend Developer with over 2 years of experience in the
-        software industry.
-      </p>
+      <div className="text-center w-full">
+        <TitleText
+          value="HELLO!"
+          size="xlarge"
+          className="text-white/90 font-black"
+        />
+        <p className="italic text-white/90">
+          I&#39;m a Backend Developer with over 2 years of experience in the
+          software industry.
+        </p>
+      </div>
     </div>
   );
 }
